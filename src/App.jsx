82c2e54bc09e4547959a976c49f79c583c478cb5
Larry Reaux <larryreaux@gmail.com>
2024-01-04 21:45:29 -0600

    updated styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import SignUpForm from "./components/SignUpForm";
 import Authenticate from "./components/Authenticate";
 import "./App.css";
+import "./index.css";
 
 function App() {
   const [token, setToken] = useState(null);
